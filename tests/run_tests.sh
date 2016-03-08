4d66c5a62d8aef1a300a4c92ddfac2a41050e753
Thomas Cuthbert <tcuthbert90@gmail.com>
2016-03-09 04:54:25 +0800

    Updated tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Run unit tests
+/etc/trigger/unit_test.sh
+
 # Test struct commando
 python /tests/test_structured_commando.py
 
 # Test gnng
-#/tests/test_gnng.sh
+/tests/test_gnng.sh
--- tests/test_gnng.sh
+++ tests/test_gnng.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/etc/trigger/bin/gnng r1.demo.local
+/etc/trigger/bin/gnng r1.demo.local 2>&1
