68cfba5c49edddbb22fb15fb824912612f9a49ec
Thomas Cuthbert <tcuthbert90@gmail.com>
2016-03-09 16:10:56 +1100

    Pretty good test runner implementation

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Run unit tests
-/etc/trigger/unit_test.sh
-
 # Test struct commando
 python /tests/test_structured_commando.py
 
 # Test gnng
 /tests/test_gnng.sh
+
+# Run unit tests
+TEST_ROOT=/etc/trigger/tests
+TEST_DATA=$TEST_ROOT/data
+export TRIGGER_SETTINGS="${TEST_DATA}/settings.py"
+cd /etc/trigger
+py.test -vv --strict
--- tests/test_gnng.sh
+++ tests/test_gnng.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 /etc/trigger/bin/gnng r1.demo.local 2>&1
+/etc/trigger/bin/gnng arista-sw1 2>&1
